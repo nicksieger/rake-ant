91b1cc344cfd249065f13c1c6f01a913cd301067
Nick Sieger <nick@nicksieger.com>
2010-01-14 11:56:05 -0500

    Cucumber feature tweaks

--- features/steps/calculator_steps.rb
+++ features/steps/calculator_steps.rb
@@ -1,15 +1,15 @@
 import Java::ruby.Calculator
 
-Given /^I have entered (\d+) into the calculator$/ do |n|
-  pending
-#   @calculator ||= Calculator.new
-#   @calculator.push n.to_i
-end
-
-When /^I press add$/ do
-  pending
-end
-
-Then /^the result should be (\d+) on the screen$/ do |n|
-  pending
-end
+# Given /^I have entered (\d+) into the calculator$/ do |n|
+#   pending
+# #   @calculator ||= Calculator.new
+# #   @calculator.push n.to_i
+# end
+# 
+# When /^I press add$/ do
+#   pending
+# end
+# 
+# Then /^the result should be (\d+) on the screen$/ do |n|
+#   pending
+# end
