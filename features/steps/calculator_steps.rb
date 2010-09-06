1faf12ac3339c3d1b1349999c392c172bba16fd3
Nick Sieger <nick@nicksieger.com>
2010-09-06 14:24:24 -0500

    Working calculator that passes features

--- features/steps/calculator_steps.rb
+++ features/steps/calculator_steps.rb
@@ -1,15 +1,14 @@
 import Java::ruby.Calculator
 
-# Given /^I have entered (\d+) into the calculator$/ do |n|
-#   pending
-# #   @calculator ||= Calculator.new
-# #   @calculator.push n.to_i
-# end
-# 
-# When /^I press add$/ do
-#   pending
-# end
-# 
-# Then /^the result should be (\d+) on the screen$/ do |n|
-#   pending
-# end
+Given /^I have entered (\d+) into the calculator$/ do |n|
+  @calculator ||= Calculator.new
+  @calculator.push n.to_i
+end
+
+When /^I press add$/ do
+  @calculator.add
+end
+
+Then /^the result should be (\d+) on the screen$/ do |n|
+  @calculator.result.should == n.to_i
+end
--- src/main/java/ruby/Calculator.java
+++ src/main/java/ruby/Calculator.java
@@ -1,4 +1,22 @@
 package ruby;
 
 public class Calculator {
+    private Stack<Integer> stack = new Stack<Integer>();
+
+    public void push(int i) {
+        stack.push(i);
+    }
+
+    public void add() {
+        Integer total = 0;
+        while (!stack.isEmpty()) {
+            total += stack.pop();
+        }
+        stack.push(total);
+    }
+
+    public int getResult() {
+        return stack.peek();
+    }
+
 }
